e19a7e522e2ba989bb3cc5b5affeac9712b52d2f
tostu <torge.stubbe@googlemail.com>
2025-08-14 00:51:28 +0200

    skill mask

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -14,9 +14,13 @@
         SiNextdotjs,
         SiJquery,
         SiNodedotjs,
+        SiLinux,
+        SiGit,
     } from "@icons-pack/svelte-simple-icons";
 
-    const skillsChoice = [
+    let isCollapseOpen = false;
+
+    const skillsMain = [
         {
             title: "Svelte",
             icon: SiSvelte,
@@ -32,23 +36,30 @@
             icon: SiTailwindcss,
             color: "#437973",
         },
-    ];
-
-    const skillsMain = [
-        {
-            title: "Docker",
-            icon: SiDocker,
-            color: "#3F6B8C",
-        },
-        {
-            title: "Spring Boot",
-            icon: SiSpring,
-            color: "#587A44",
-        },
         {
             title: "Node.js",
             icon: SiNodedotjs,
             color: "#339933",
+        },
+        {
+            title: "Docker",
+            icon: SiDocker,
+            color: "#2496ED",
+        },
+        {
+            title: "Linux",
+            icon: SiLinux,
+            color: "#000000",
+        },
+        {
+            title: "TypeScript",
+            icon: SiTypescript,
+            color: "#3178C6",
+        },
+        {
+            title: "Git",
+            icon: SiGit,
+            color: "#F05032",
         },
     ];
 
@@ -117,24 +128,30 @@
 </script>
 
 <div class="flex flex-col gap-6">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {#each skillsChoice as skill}
-            <div class="card bg-base-100 shadow-sm">
-                <div class="card-body">
-                    <div class="flex flex-col items-center gap-2">
-                        <svelte:component
-                            this={skill.icon}
-                            color={skill.color}
-                            size={90}
-                        />
-                        <h3>{skill.title}</h3>
+    <div
+        class={isCollapseOpen
+            ? ""
+            : "mask-b-from-60% mask-b-to-95% lg:mask-b-from-20% lg:mask-b-to-80%"}
+    >
+        <div class="grid grid-cols-2 lg:grid-cols-4 gap-5">
+            {#each skillsMain as skill}
+                <div class="card bg-base-100 shadow-sm text-center">
+                    <div class="card-body">
+                        <div class="flex flex-col items-center gap-2">
+                            <svelte:component
+                                this={skill.icon}
+                                color={skill.color}
+                                size={80}
+                            />
+                            <h3>{skill.title}</h3>
+                        </div>
                     </div>
                 </div>
-            </div>
-        {/each}
+            {/each}
+        </div>
     </div>
     <div class="collapse">
-        <input type="checkbox" />
+        <input type="checkbox" bind:checked={isCollapseOpen} />
         <div
             class="collapse-title font-semibold p-0 text-center flex flex-col justify-center"
         >
@@ -144,46 +161,24 @@
         <div class="collapse-content p-0 text-sm">
             <div class="flex flex-col gap-10">
                 <div>
-                    <h2>Main Skills</h2>
-                    <div
-                        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
-                    >
-                        {#each skillsMain as skill}
-                            <div class="card bg-base-100 shadow-sm">
-                                <div class="card-body">
-                                    <div
-                                        class="flex flex-col items-center gap-2"
-                                    >
-                                        <svelte:component
-                                            this={skill.icon}
-                                            color={skill.color}
-                                            size={90}
-                                        />
-                                        <h3>{skill.title}</h3>
-                                    </div>
-                                </div>
-                            </div>
-                        {/each}
-                    </div>
-                </div>
-
-                <div>
                     <h2>Additional Skills</h2>
                     <div
-                        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
+                        class="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-4 gap-5"
                     >
                         {#each skillsAdditional as skill}
                             <div class="card bg-base-100 shadow-sm">
-                                <div class="card-body">
+                                <div class="card-body text-center">
                                     <div
                                         class="flex flex-col items-center gap-2"
                                     >
                                         <svelte:component
                                             this={skill.icon}
                                             color={skill.color}
-                                            size={90}
+                                            size={50}
                                         />
-                                        <h3>{skill.title}</h3>
+                                        <h4>
+                                            {skill.title}
+                                        </h4>
                                     </div>
                                 </div>
                             </div>
@@ -194,14 +189,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    .mask1 {
-        /*-webkit-mask-image: linear-gradient(
-            to bottom,
-            transparent 25%,
-            black 75%
-        );*/
-        mask-image: linear-gradient(to bottom, black 60%, transparent 50%);
-    }
-</style>
